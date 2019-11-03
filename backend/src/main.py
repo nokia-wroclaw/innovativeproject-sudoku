13bd47ddfcfe570807b9b274f5e98047d8e30f02
mpodgorny <m.podgorny2@gmail.com>
2019-11-03 01:59:26 +0100

    Basic login utility and new project structure with modules

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,8 +1,15 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, Header, HTTPException
+
+from .routers import login
 
 app = FastAPI()
 
+async def get_token_header(x_token: str = Header(...)):
+    if x_token != "fake-super-secret-token":
+        raise HTTPException(status_code=400, detail="X-Token header invalid")
 
-@app.get("/")
+@router.get("/")
 async def root():
     return {"message": "Hello World!"}
+
+app.include_router(login.router)
