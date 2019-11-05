82ac8b53763e1663dbc338561df609d79acf4f9a
Pawel Rubin <pawelrubin19@gmail.com>
2019-11-05 18:08:32 +0100

    login-register-view rebased into master

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+<<<<<<< HEAD
 import "./App.css";
 import Init from "./components/Init/Init";
 
@@ -10,6 +11,27 @@ function App() {
       </header>
     </div>
   );
+=======
+import "./App.scss";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter, Route, Redirect} from "react-router-dom";
+import Register from "./components/Auth/Register/Register";
+import Login from "./components/Auth/Login/Login";
+
+function App() {
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <div>
+                    <Redirect exact from="/" to="/register" />
+                    <Route path="/register" component={Register} />
+                    <Route path="/login" component={Login} />
+                    <Route path="/game" />
+                </div>
+            </BrowserRouter>
+        </div>
+    );
+>>>>>>> 5a86a30... login-register view before rebase
 }
 
 export default App;
---
+++ frontend/src/components/Auth/Register/Register.js
@@ -0,0 +1,106 @@
+import "../Auth.scss";
+import React, {Component} from "react";
+import {Link} from 'react-router-dom';
+import TextField from "@material-ui/core/TextField";
+import Button from '@material-ui/core/Button';
+
+class Register extends Component {
+
+    isFormValid = false;
+
+    state = {
+        email: {
+            invalid: false,
+            value: ''
+        },
+        password: {
+            invalid: false,
+            value: ''
+        },
+        confirmPassword: {
+            invalid: false,
+            value: ''
+        }
+    };
+
+    onInput = event => {
+        const name = event.target.name;
+        const value = event.target.value;
+        this.setState({
+            [name]: {
+                invalid: value === "",
+                value: value
+            }
+        }, () => {
+            this.isFormValid =
+                !this.state.email.invalid &&
+                !this.state.password.invalid &&
+                !this.state.confirmPassword.invalid
+        });
+    };
+
+    onSubmit = event => {
+        event.preventDefault();
+        console.log(this.state)
+    };
+
+    render() {
+        return (
+            <div className="card">
+                <h1>Sudoku Battle Royale</h1>
+                <form className="container" onSubmit={this.onSubmit}>
+                    <div>
+                        <TextField
+                            name="email"
+                            error={this.state.email.invalid}
+                            label="Email"
+                            defaultValue={this.state.email.value}
+                            onChange={this.onInput}
+                            onBlur={this.onInput}
+                            className="textField"
+                            margin="normal"
+                        />
+                    </div>
+                    <div>
+                        <TextField
+                            name="password"
+                            error={this.state.password.invalid}
+                            label="Password"
+                            type="password"
+                            defaultValue={this.state.password.value}
+                            onChange={this.onInput}
+                            onBlur={this.onInput}
+                            className="textField"
+                            margin="normal"
+                        />
+                    </div>
+                    <div>
+                        <TextField
+                            name="confirmPassword"
+                            error={this.state.confirmPassword.invalid}
+                            label="Confirm password"
+                            type="password"
+                            defaultValue={this.state.confirmPassword.value}
+                            onChange={this.onInput}
+                            onBlur={this.onInput}
+                            className="textField"
+                            margin="normal"
+                        />
+                    </div>
+                    <Button
+                        type="submit"
+                        variant="outlined"
+                        size="large"
+                        className="mt-3 mb-3"
+                        disabled={!this.isFormValid}
+                    >
+                        Sign Up
+                    </Button>
+                </form>
+                <Link to="/login">Already have account?</Link>
+            </div>
+        )
+    }
+}
+
+export default Register;
