00374031345a0b32f695a5a0c1373aaa9da5888b
Paweł Rubin <32072144+pawelrubin@users.noreply.github.com>
2019-11-05 15:37:09 +0100

    WIP: Frontend Travis (#17)
    Additions
      - eslint with prettier and airbnb-config installed
      - hasky pre-commit hook for prettier.js
      - Travis setup for linter and prettier
    
    Changes
      - eslint errors resolved in code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import Init from "./components/Init/Init";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Init text="Sudoku Battle Royale"/>
+        <Init text="Sudoku Battle Royale" />
       </header>
     </div>
   );
--- frontend/src/__tests__/Init.test.js
+++ frontend/src/__tests__/Init.test.js
@@ -1,14 +1,13 @@
-import React from 'react';
-import {render} from '@testing-library/react';
-import Init from "../components/Init/Init"
+import React from "react";
+import { render } from "@testing-library/react";
+import Init from "../components/Init/Init";
 
-
-it('renders', () => {
-    const {asFragment} = render(<Init text="Sudoku Battle Royale"/>);
-    expect(asFragment()).toMatchSnapshot();
+it("renders", () => {
+  const { asFragment } = render(<Init text="Sudoku Battle Royale" />);
+  expect(asFragment()).toMatchSnapshot();
 });
 
 it("inserts text in h1", () => {
-    const {getByTestId} = render(<Init text="Sudoku Battle Royale"/>);
-    expect(getByTestId('text')).toHaveTextContent('Sudoku Battle Royale');
+  const { queryByTestId } = render(<Init text="Sudoku Battle Royale" />);
+  expect(queryByTestId("text")).toHaveTextContent("Sudoku Battle Royale");
 });
