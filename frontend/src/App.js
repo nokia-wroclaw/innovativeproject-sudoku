0eb92a61b73e47ae395517decb832af82e0e258d
Karol Szymończyk <karolszymonczyk@gmail.com>
2019-11-06 13:59:10 +0100

    Login/Register views added: - created Login and Register classes - React Router installed - App.js as Routing Module - Simple routes added - Eslint configuration changed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,23 @@
 import React from "react";
-<<<<<<< HEAD
-import "./App.css";
-import Init from "./components/Init/Init";
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Init text="Sudoku Battle Royale" />
-      </header>
-    </div>
-  );
-=======
 import "./App.scss";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {BrowserRouter, Route, Redirect} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Route, Redirect } from "react-router-dom";
 import Register from "./components/Auth/Register/Register";
 import Login from "./components/Auth/Login/Login";
 
 function App() {
-    return (
-        <div className="App">
-            <BrowserRouter>
-                <div>
-                    <Redirect exact from="/" to="/register" />
-                    <Route path="/register" component={Register} />
-                    <Route path="/login" component={Login} />
-                    <Route path="/game" />
-                </div>
-            </BrowserRouter>
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <div>
+          <Redirect exact from="/" to="/register" />
+          <Route path="/register" component={Register} />
+          <Route path="/login" component={Login} />
+          <Route path="/game" />
         </div>
-    );
->>>>>>> 5a86a30... login-register view before rebase
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
--- frontend/src/components/Auth/Login/Login.js
+++ frontend/src/components/Auth/Login/Login.js
@@ -1,86 +1,89 @@
 import "../Auth.scss";
-import React, {Component} from "react";
+import React, { Component } from "react";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 class Login extends Component {
+  isFormValid = false;
 
-    isFormValid = false;
-
-    state = {
-        email: {
-            invalid: false,
-            value: ''
-        },
-        password: {
-            invalid: false,
-            value: ''
-        }
-    };
-
-    onInput = event => {
-        const name = event.target.name;
-        const value = event.target.value;
-        this.setState({
-            [name]: {
-                invalid: value === "",
-                value: value
-            }
-        }, () => {
-            this.isFormValid = !this.state.email.invalid && !this.state.password.invalid
-        });
-    };
-
-    onSubmit = event => {
-        event.preventDefault();
-        console.log(this.state)
-    };
-
-    render() {
-        return (
-            <div className="card">
-                <h1>Sudoku Battle Royale</h1>
-                <form className="container" onSubmit={this.onSubmit} id="form">
-                    <div>
-                        <TextField
-                            name="email"
-                            error={this.state.email.invalid}
-                            label="Email"
-                            defaultValue={this.state.email.value}
-                            onChange={this.onInput}
-                            onBlur={this.onInput}
-                            className="textField"
-                            margin="normal"
-                        />
-                    </div>
-                    <div>
-                        <TextField
-                            name="password"
-                            error={this.state.password.invalid}
-                            label="Password"
-                            type="password"
-                            defaultValue={this.state.password.value}
-                            onChange={this.onInput}
-                            onBlur={this.onInput}
-                            className="textField"
-                            margin="normal"
-                        />
-                    </div>
-                    <Button
-                        type="submit"
-                        variant="outlined"
-                        size="large"
-                        className="mt-3 mb-3"
-                        disabled={!this.isFormValid}
-                    >
-                        Sign In
-                    </Button>
-                </form>
-                <Link to="/register">New user? Register here!</Link>
-            </div>
-        )
+  state = {
+    email: {
+      invalid: false,
+      value: ""
+    },
+    password: {
+      invalid: false,
+      value: ""
     }
+  };
+
+  onInput = event => {
+    const [name] = event.target.name;
+    const [value] = event.target.value;
+    this.setState(
+      {
+        [name]: {
+          invalid: value === "",
+          value
+        }
+      },
+      () => {
+        this.isFormValid =
+          !this.state.email.invalid && !this.state.password.invalid;
+      }
+    );
+  };
+
+  onSubmit = event => {
+    event.preventDefault();
+    console.log(this.state);
+  };
+
+  render() {
+    return (
+      <div className="card">
+        <h1>Sudoku Battle Royale</h1>
+        <form className="container" onSubmit={this.onSubmit} id="form">
+          <div>
+            <TextField
+              name="email"
+              error={this.state.email.invalid}
+              label="Email"
+              defaultValue={this.state.email.value}
+              onChange={this.onInput}
+              onBlur={this.onInput}
+              className="textField"
+              margin="normal"
+            />
+          </div>
+          <div>
+            <TextField
+              name="password"
+              error={this.state.password.invalid}
+              label="Password"
+              type="password"
+              defaultValue={this.state.password.value}
+              onChange={this.onInput}
+              onBlur={this.onInput}
+              className="textField"
+              margin="normal"
+            />
+          </div>
+          <Button
+            type="submit"
+            variant="outlined"
+            size="large"
+            className="mt-3 mb-3"
+            disabled={!this.isFormValid}
+          >
+            Sign In
+          </Button>
+        </form>
+        <Link to="/register">New user? Register here!</Link>
+      </div>
+    );
+  }
 }
 
 export default Login;
