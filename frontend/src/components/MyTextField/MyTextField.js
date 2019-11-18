bc85747ea30cdd9a343f969e39e15679b7bbe8ad
Karol Szymończyk <karolszymonczyk@gmail.com>
2019-11-18 01:24:45 +0100

    Login/Register views edited: - Formik installed - Validation using yup library - MyTextField component added

---
+++ frontend/src/components/MyTextField/MyTextField.js
@@ -0,0 +1,29 @@
+import React from "react";
+import * as PropTypes from "prop-types";
+import { useField } from "formik";
+import TextField from "@material-ui/core/TextField";
+
+const MyTextField = ({ label, ...props }) => {
+  const [field, meta] = useField(props);
+  const errorText = meta.error && meta.touched ? meta.error : "";
+  return (
+    <TextField
+      {...field}
+      label={label}
+      helperText={errorText}
+      error={!!errorText}
+      className="textField"
+      margin="dense"
+    />
+  );
+};
+
+MyTextField.propTypes = {
+  label: PropTypes.string
+};
+
+MyTextField.defaultProps = {
+  label: ""
+};
+
+export default MyTextField;
