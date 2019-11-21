bd8c88a2e3fda1059c33a24f5f71f4fda589f29c
karolszymonczyk <43317648+karolszymonczyk@users.noreply.github.com>
2019-11-21 07:44:49 +0100

    Frontend/feature/login register view (#29)
    * Login/Register views
      - formik forms (https://github.com/jaredpalmer/formik)
      - validation with yup (https://github.com/jquense/yup)
    
    * Render sudoku board
      - responsive sudoku grid
      - draggable tiles (https://github.com/react-dnd/react-dnd/)

---
+++ frontend/src/components/Auth/Login/Login.js
@@ -0,0 +1,64 @@
+import "../Auth.scss";
+import React from "react";
+import Button from "@material-ui/core/Button";
+import { Link } from "react-router-dom";
+import { Formik, Form } from "formik";
+import * as yup from "yup";
+import TextField from "../../TextField/TextField";
+
+const form = {
+  email: "",
+  password: ""
+};
+
+const validationSchema = yup.object({
+  email: yup
+    .string()
+    .email()
+    .required(),
+  password: yup.string().required()
+});
+
+const onSubmit = (data, setSubmitting) => {
+  setSubmitting(true);
+  console.log(data);
+  setSubmitting(false);
+};
+
+const Login = () => {
+  return (
+    <div className="Auth">
+      <div className="card">
+        <h1>Sudoku Battle Royale</h1>
+        <Formik
+          initialValues={form}
+          validationSchema={validationSchema}
+          onSubmit={(data, { setSubmitting }) => {
+            onSubmit(data, setSubmitting);
+          }}
+        >
+          {({ isSubmitting }) => (
+            <Form className="container">
+              <div className="textFieldWrapper">
+                <TextField label="Email" name="email" type="email" />
+                <TextField label="Password" name="password" type="password" />
+              </div>
+              <Button
+                type="submit"
+                variant="outlined"
+                size="large"
+                className="mt-5 mb-3"
+                disabled={isSubmitting}
+              >
+                Sign In
+              </Button>
+            </Form>
+          )}
+        </Formik>
+        <Link to="/register">New user? Register here!</Link>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
