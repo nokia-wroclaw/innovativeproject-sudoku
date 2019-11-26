2b9ff9c045806bcdb87b2df3749c58c4ddf4de5f
Pawel Rubin <pawelrubin19@gmail.com>
2019-11-27 00:00:41 +0100

    Generating complete sudoku boards.

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 
+from .sudokuboard import SudokuBoard
+
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World!"}
+
+
+@app.get("/sudoku")
+async def sudoku():
+    board = SudokuBoard()
+    return {"sudokuBoard": board.get_board_matrix()}
