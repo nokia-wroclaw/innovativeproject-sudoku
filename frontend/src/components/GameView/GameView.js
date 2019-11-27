9b240b4aee842cb86d7dca1346e1b794abfce12f
szymonglab <glab.bro@gmail.com>
2019-11-27 23:45:27 +0100

    Created timer.

--- frontend/src/components/GameView/GameView.js
+++ frontend/src/components/GameView/GameView.js
@@ -2,6 +2,8 @@ import React from "react";
 import HTML5Backend from "react-dnd-html5-backend";
 import { DndProvider } from "react-dnd";
 import "./GameView.scss";
+import Timer from "./Timer/Timer";
+import "../../Variables.scss";
 import Board from "../Board/Board";
 import DragPanel from "../Draggable/DragPanel/DragPanel";
 
@@ -17,13 +19,22 @@ export default function GameView() {
     [1, 2, 3, 4, 5, 6, 7, 8, 9],
     [1, 2, 3, 4, 5, 6, 7, 8, 9]
   ];
+  const mockTime = { max_time: 300, current_time: 6 };
 
   return (
-    <DndProvider backend={HTML5Backend}>
-      <div className="gamePanel">
-        <Board fields={mockBoard} />
-        <DragPanel />
+    <div className="gameView">
+      <div>
+        <Timer
+          currentTime={mockTime.current_time}
+          maxTime={mockTime.max_time}
+        />
       </div>
-    </DndProvider>
+      <DndProvider backend={HTML5Backend}>
+        <div className="gamePanel">
+          <Board fields={mockBoard} />
+          <DragPanel />
+        </div>
+      </DndProvider>
+    </div>
   );
 }
