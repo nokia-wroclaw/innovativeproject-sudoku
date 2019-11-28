e42e6c0c0252c4e1506b4a270f5d5317daa6a126
mpodgorny <m.podgorny2@gmail.com>
2019-11-28 02:32:11 +0100

    Minor bugfix.

--- backend/src/routers/user_auth/get_token.py
+++ backend/src/routers/user_auth/get_token.py
@@ -1,6 +1,7 @@
 from datetime import timedelta
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from fastapi.encoders import jsonable_encoder
+from starlette.status import HTTP_403_FORBIDDEN
 from starlette.requests import Request
 from starlette.responses import Response
 from .utils import (
@@ -17,7 +18,10 @@ router = APIRouter()
 async def get_acces_token(request: Request):
     refresh_token = request.cookies.get("refresh_token")
     response = Response()
-
+    if refresh_token is None:
+        raise HTTPException(
+            status_code=HTTP_403_FORBIDDEN, detail="No rights to refresh token.",
+        )
     username = verify_refresh_token(refresh_token)
     if username:
         access_token_expires = timedelta(minutes=ACCES_TOKEN_EXPIRES_MINUTES)
