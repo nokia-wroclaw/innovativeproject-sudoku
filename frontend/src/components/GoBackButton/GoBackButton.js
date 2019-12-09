d8afc9b87167196e909e05f1eb4baa5a8527b275
Karol Szymończyk <karolszymonczyk@gmail.com>
2019-12-09 15:01:56 +0100

    useNavigation hook added to control modal and history

--- frontend/src/components/GoBackButton/GoBackButton.js
+++ frontend/src/components/GoBackButton/GoBackButton.js
@@ -1,37 +1,30 @@
 import "./GoBackButton.scss";
-import React, { useState } from "react";
-import { IconButton } from "@material-ui/core";
+import React from "react";
 import { useHistory } from "react-router";
+import { IconButton } from "@material-ui/core";
 import ConfirmationDialog from "../ConfirmationDialog/ConfirmationDialog";
-
-const handler = (history, setOpen) => {
-  if (history.location.pathname === "/game") {
-    setOpen(true);
-  } else {
-    history.goBack();
-  }
-};
-
-const handleSuccess = (history, setOpen) => {
-  history.goBack();
-  setOpen(false);
-};
+import useNavigation from "../../hooks/useNavigation";
 
 const GoBackButton = () => {
+  const [open, setOpen] = useNavigation("/game");
   const history = useHistory();
-  const [open, setOpen] = useState(false);
+
+  const handleSuccess = () => {
+    setOpen(false);
+    history.goBack();
+  };
 
   return (
     <div className="GoBackButton">
       <ConfirmationDialog
         title="Warning!"
         open={open}
-        success={() => handleSuccess(history, setOpen)}
+        success={() => handleSuccess()}
         fail={() => setOpen(false)}
       >
         Are you sure you want to leave the game?
       </ConfirmationDialog>
-      <IconButton onClick={() => handler(history, setOpen)}>
+      <IconButton onClick={() => setOpen(true)}>
         <i className="fas fa-arrow-left" />
       </IconButton>
     </div>
