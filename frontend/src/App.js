db1f5f347d8b4464f8f591c89ac66008c6a0472c
karolszymonczyk <43317648+karolszymonczyk@users.noreply.github.com>
2019-12-15 18:10:45 +0100

    Frontend/animating background (#50)
    * particles.js animated background
    
    * TextField and autocomplete style
    
    * -theme moved to App.js to globaly controll MUI
    
    * numbers in particle.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,82 @@
 import React from "react";
 import "./App.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
-// import "@fortawesome/fontawesome-free/css/all.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
+import { MuiThemeProvider } from "@material-ui/core/styles";
 import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 import Register from "./components/Auth/Register/Register";
 import Login from "./components/Auth/Login/Login";
 import Menu from "./components/Menu/Menu";
 import GameView from "./components/GameView/GameView";
 import Settings from "./components/Settings/Settings";
+import ParticlesComponent from "./components/ParticlesComponent/ParticlesComponent";
+
+const theme = createMuiTheme({
+  overrides: {
+    MuiInputLabel: {
+      root: {
+        color: "white",
+        "&$focused": {
+          color: "white",
+          "&$error": {
+            color: "#f44336"
+          }
+        }
+      }
+    },
+    MuiInput: {
+      root: {
+        color: "white"
+      },
+      underline: {
+        "&:after": {
+          borderBottomColor: "white"
+        },
+        "&:before": {
+          borderBottomColor: "white"
+        },
+        "&:hover:not($disabled):not($focused):not($error):before": {
+          borderBottomColor: "white"
+        }
+      }
+    },
+    MuiButton: {
+      outlined: {
+        borderColor: "white",
+        color: "white",
+        "&:hover": {
+          backgroundColor: "white",
+          color: "black"
+        }
+      }
+    }
+  }
+});
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <>
-          <Switch>
-            <Route exact path="/">
-              <Redirect to="register" />
-            </Route>
-            <Route path="/register" component={Register} />
-            <Route path="/login" component={Login} />
-            <Route path="/menu" component={Menu} />
-            <Route path="/game" component={GameView} />
-            <Route path="/settings" component={Settings} />
-            <Route path="*">
-              <Redirect to="register" />
-            </Route>
-          </Switch>
-        </>
-      </BrowserRouter>
+      <ParticlesComponent />
+      <div className="foreground">
+        <MuiThemeProvider theme={theme}>
+          <BrowserRouter>
+            <Switch>
+              <Route exact path="/">
+                <Redirect to="register" />
+              </Route>
+              <Route path="/register" component={Register} />
+              <Route path="/login" component={Login} />
+              <Route path="/menu" component={Menu} />
+              <Route path="/game" component={GameView} />
+              <Route path="/settings" component={Settings} />
+              <Route path="*">
+                <Redirect to="register" />
+              </Route>
+            </Switch>
+          </BrowserRouter>
+        </MuiThemeProvider>
+      </div>
     </div>
   );
 }
