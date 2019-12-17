d3322bc1789b7c0cc0ae835167a49c1cf61eb31b
mpodgorny <m.podgorny2@gmail.com>
2019-12-17 23:36:20 +0100

    User model improvement

--- backend/src/models.py
+++ backend/src/models.py
@@ -2,7 +2,7 @@ from mongoengine import StringField, BooleanField, Document
 
 
 class User(Document):
-    username = StringField(required=True, max_length=200)
-    email = StringField(required=True, max_length=200)
+    username = StringField(required=True, max_length=200, unique=True)
+    email = StringField(required=True, max_length=200, unique=True)
     hashed_password = StringField(required=True, max_length=200)
     disabled = BooleanField(default=False)
