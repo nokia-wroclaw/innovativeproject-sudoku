a29e5e6d91e7299c534574857515a553fba9a02d
mpodgorny <m.podgorny2@gmail.com>
2019-12-17 23:47:05 +0100

    Email field instead of string field in model

--- backend/src/models.py
+++ backend/src/models.py
@@ -1,8 +1,8 @@
-from mongoengine import StringField, BooleanField, Document
+from mongoengine import StringField, BooleanField, EmailField, Document
 
 
 class User(Document):
     username = StringField(required=True, max_length=200, unique=True)
-    email = StringField(required=True, max_length=200, unique=True)
+    email = EmailField(required=True, max_length=200, unique=True)
     hashed_password = StringField(required=True, max_length=200)
     disabled = BooleanField(default=False)
