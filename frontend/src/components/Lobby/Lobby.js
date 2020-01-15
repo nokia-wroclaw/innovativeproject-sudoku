7b07e5e0302ad8e31e717ec10ea8a085a3fb0a30
Karol Szymończyk <karolszymonczyk@gmail.com>
2020-01-15 21:07:34 +0100

    lobby view created

---
+++ frontend/src/components/Lobby/Lobby.js
@@ -0,0 +1,18 @@
+import "./Lobby.scss";
+import React from "react";
+import PropTypes from "prop-types";
+
+const Lobby = props => {
+  return (
+    <div className="Lobby">
+      <div className="card">
+        <img src="logo.png" alt="logo_image" />
+        <h1>Waiting for players...</h1>
+      </div>
+    </div>
+  );
+};
+
+Lobby.propTypes = {};
+
+export default Lobby;
