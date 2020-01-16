391458cf27c5a47a1834d8174b7601dd3f2b5a01
Karol Szymończyk <karolszymonczyk@gmail.com>
2020-01-16 20:45:48 +0100

    -Lobby screen for 9 players -CSS loader -Buttons fixed

--- frontend/src/components/Lobby/Lobby.js
+++ frontend/src/components/Lobby/Lobby.js
@@ -1,18 +1,65 @@
 import "./Lobby.scss";
 import React from "react";
-import PropTypes from "prop-types";
+import Loader from "../Loader/Loader";
+import { useState } from "react";
+import { useHistory } from "react-router";
+import { Button, Table, TableRow, TableCell } from "@material-ui/core";
 
 const Lobby = () => {
+  const history = useHistory();
+  const [start, setstart] = useState(false);
+
+  const init = ["Player1", "Player2", "Player3", "-", "-", "-", "-", "-"];
+
+  const [players, setPlayers] = useState(init);
+
+  const redirect = path => {
+    history.push(path);
+  };
+
+  const displayPlayers = () => {
+    return (
+      <div className="players">
+        <p>1. MyUsername</p>
+        <div className="columns">
+          <Table size="small">
+            {players.map((player, index) => {
+              return (
+                <TableRow>
+                  <TableCell style={{ width: "1px", padding: "0" }}>
+                    {index + 2}.
+                  </TableCell>
+                  <TableCell align="center" style={{ paddingRight: "30px" }}>
+                    {player}
+                  </TableCell>
+                </TableRow>
+              );
+            })}
+          </Table>
+        </div>
+      </div>
+    );
+  };
+
   return (
     <div className="Lobby">
       <div className="card">
         <img src="logo.png" alt="logo_image" />
-        <h1>Waiting for players...</h1>
+        <h2>Waiting for players...</h2>
+        {/* <h1>1 / 9</h1> */}
+        {displayPlayers()}
+        <Loader></Loader>
+        <Button
+          disable="start"
+          size="large"
+          variant="outlined"
+          onClick={() => redirect("/menu")}
+        >
+          Cancel
+        </Button>
       </div>
     </div>
   );
 };
-
-Lobby.propTypes = {};
 
 export default Lobby;
