408676823fee0ad92cdbbdd63753316a60178740
Paweł Rubin <pawelrubin19@gmail.com>
2020-01-20 13:46:42 +0100

    black formatting applied.

--- backend/src/auth.py
+++ backend/src/auth.py
@@ -59,7 +59,7 @@ def connect_to_db():
             password=DB_PASSWORD,
             authentication_source="admin",
             host="mongo-container",
-            port=27017
+            port=27017,
         )
     except ConnectionError as error:
         logging.error(error)
