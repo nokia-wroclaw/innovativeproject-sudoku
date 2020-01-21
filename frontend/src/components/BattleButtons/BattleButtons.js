19da2547f0df363354a093c387b70d5e12cbef3d
Karol Szymończyk <karolszymonczyk@gmail.com>
2020-01-21 14:19:23 +0100

    -Fight and heal buttons -Fight and heal images -Style bugs fixed

---
+++ frontend/src/components/BattleButtons/BattleButtons.js
@@ -0,0 +1,18 @@
+import "./BattleButtons.scss";
+
+import React from "react";
+
+const BattleButtons = () => {
+  return (
+    <div className="BattleButtons">
+      <div className="bButton bHeal">
+        <img />
+      </div>
+      <div className="bButton bFight">
+        <img />
+      </div>
+    </div>
+  );
+};
+
+export default BattleButtons;
