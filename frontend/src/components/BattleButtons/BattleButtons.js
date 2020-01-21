33cc14b81f3e4ada7e2bb287ba6c92fd083b6092
Karol Szymończyk <karolszymonczyk@gmail.com>
2020-01-21 17:24:13 +0100

    -Fight and heal buttons -Fight and heal images -Style bugs fixed -Board reload fixed

--- frontend/src/components/BattleButtons/BattleButtons.js
+++ frontend/src/components/BattleButtons/BattleButtons.js
@@ -1,18 +1,34 @@
 import "./BattleButtons.scss";
-
+import PropTypes from "prop-types";
 import React from "react";
 
-const BattleButtons = () => {
+const BattleButtons = ({ downloadNewBoard }) => {
   return (
     <div className="BattleButtons">
-      <div className="bButton bHeal">
-        <img />
+      <div
+        className="bButton bHeal"
+        role="button"
+        tabIndex="0"
+        onClick={downloadNewBoard}
+        onKeyDown={downloadNewBoard}
+      >
+        <img alt="heal" />
       </div>
-      <div className="bButton bFight">
-        <img />
+      <div
+        className="bButton bFight"
+        role="button"
+        tabIndex="0"
+        onClick={downloadNewBoard}
+        onKeyDown={downloadNewBoard}
+      >
+        <img alt="fight" />
       </div>
     </div>
   );
+};
+
+BattleButtons.propTypes = {
+  downloadNewBoard: PropTypes.func.isRequired
 };
 
 export default BattleButtons;
