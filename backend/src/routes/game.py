7859dd89833a4a70205791a6ca6732d14d4b2bd9
Michał Podgórny <39701478+mpodgorny@users.noreply.github.com>
2020-01-26 15:08:45 +0100

    Game stats (#98)

--- backend/src/routes/game.py
+++ backend/src/routes/game.py
@@ -11,6 +11,7 @@ from websockets.exceptions import ConnectionClosedError
 
 from ..game import Game
 from ..auth import verify_cookies, CookieVerificationError
+from .stats import update_stats
 
 
 game_router = APIRouter()
@@ -52,6 +53,7 @@ async def websocket_endpoint(websocket: WebSocket):
         except asyncio.TimeoutError:
             await check_timers(websocket, username, game)
         except (WebSocketDisconnect, ConnectionClosedError):
+            update_stats(game.players_data[username], username, False)
             game.remove(username)
             if len(game.players) == 0 and game in games:
                 games.remove(game)
@@ -78,10 +80,12 @@ async def get_data(websocket, wrapper):
 async def check_timers(websocket, username, game):
     if len(game.usernames) == 1:
         await websocket.send_json({"code": "game_won"})
+        update_stats(game.players_data[username], username, True)
         game.remove(username)
 
     for name, player in game.players_data.copy().items():
         if player.endgame_time <= time.time():
+            update_stats(player, name, False)
             await game.players[name].send_json({"code": "game_lost"})
             logging.info("time out on player %s", username)
             game.remove(name)
---
+++ backend/src/routes/stats.py
@@ -0,0 +1,53 @@
+from time import time
+import json
+
+from fastapi import APIRouter, HTTPException
+from starlette.status import HTTP_400_BAD_REQUEST
+from starlette.responses import JSONResponse
+from starlette.requests import Request
+from mongoengine import DoesNotExist
+
+from ..models import UserStats
+from ..auth import (
+    verify_cookies,
+    CookieVerificationError,
+)
+from ..game import Player
+
+stats_router = APIRouter()
+
+
+@stats_router.get("/stats")
+async def get_stats(request: Request) -> JSONResponse:
+    try:
+        username = verify_cookies(request.cookies, "access_token")
+        user = UserStats.objects.get(username=username)
+        player_stats = json.loads(user.to_json())
+        response_json = {}
+        del player_stats["_id"]
+        response_json["top5"] = get_current_top()
+        response_json["player_stats"] = player_stats
+        return JSONResponse(response_json)
+    except (CookieVerificationError, DoesNotExist):
+        raise HTTPException(status_code=HTTP_400_BAD_REQUEST)
+
+
+def get_current_top():
+    players = UserStats.objects.order_by("-games_won").limit(5)
+    top5_dict = {}
+    for i, player in enumerate(players):
+        player_json = {}
+        player_json["username"] = player.username
+        player_json["games_won"] = player.games_won
+        top5_dict[i] = player_json
+    return top5_dict
+
+
+def update_stats(player: Player, username: str, won: bool):
+    user = UserStats.objects.get(username=username)
+    user.time_spend += time() - player.start_time
+    user.heals += player.heals
+    user.attacks += player.attacks
+    user.games_total += 1
+    user.games_won += 1 if won else 0
+    user.save()
