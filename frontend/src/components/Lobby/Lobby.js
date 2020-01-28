14bd27147d7f47e0c93afa5fed28e6e61d8a3862
Paweł Rubin <pawelrubin19@gmail.com>
2020-01-28 20:19:05 +0100

    Lobby timer 2.0

--- frontend/src/components/Lobby/Lobby.js
+++ frontend/src/components/Lobby/Lobby.js
@@ -12,14 +12,17 @@ import LoggedContext from "../../contexts/LoggedContext";
 import "react-sweet-progress/lib/style.css";
 
 const emptyPlayersList = ["-", "-", "-", "-", "-", "-", "-", "-"];
+const waitingMessage = "Waiting for players...";
+const startingMessage = "Starting game...";
 
 const Lobby = () => {
   const history = useHistory();
   const isLogged = useContext(LoggedContext);
 
   const [playersList, setPlayersList] = useState(emptyPlayersList);
-  const [lobbyTimer, setLobbyTimer] = useState(15);
-  const [message, setMessage] = useState("Waiting for players...");
+  const [lobbyTimer, setLobbyTimer] = useState(1);
+  const [message, setMessage] = useState(waitingMessage);
+  const [lobbyTimerLimit, setLobbyTimerLimit] = useState(1);
 
   useEffect(() => {
     if (!isLogged) {
@@ -47,16 +50,20 @@ const Lobby = () => {
         case "in_game_already":
           history.push("/game");
           break;
+        case "enter_lobby":
+          setLobbyTimerLimit(response.timer_limit);
+          setLobbyTimer(response.timer_limit);
+          break;
         case "players":
           setPlayersList(makePlayersList(response.players));
           break;
         case "time":
-          setMessage("Starting game...");
+          setMessage(startingMessage);
           setLobbyTimer(response.time);
           break;
         case "timer_off":
-          setMessage("Waiting for players...");
-          setLobbyTimer(15);
+          setMessage(waitingMessage);
+          setLobbyTimer(lobbyTimerLimit);
           break;
         default:
           break;
@@ -71,6 +78,12 @@ const Lobby = () => {
       ws.close();
     };
   }, [history]);
+
+  useEffect(() => {
+    if (message === waitingMessage) {
+      setLobbyTimer(lobbyTimerLimit);
+    }
+  }, [message]);
 
   const renderColumn = odd => {
     return (
@@ -94,7 +107,7 @@ const Lobby = () => {
   const displayTimer = () => {
     return (
       <Progress
-        percent={(parseInt(lobbyTimer, 10) / 15) * 100}
+        percent={(parseInt(lobbyTimer, 10) / lobbyTimerLimit) * 100}
         theme={{
           active: {
             symbol: `${parseInt(lobbyTimer, 10)}s`,
